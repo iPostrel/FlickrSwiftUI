9b7a3a28983993354c656485e2125b509f29b28c
iPostrel <60104196+iPostrel@users.noreply.github.com>
2022-05-31 21:13:18 +0300

    Initial commit

---
+++ FlickrSwiftUI/FlickrSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSwiftUIApp.swift
+//  FlickrSwiftUI
+//
+//  Created by Денис on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlickrSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FlickrSwiftUI
+ This is a demo app where you can see that how you can to use Flickr service api to get and show etc..  www.flickr.com/services/api/
